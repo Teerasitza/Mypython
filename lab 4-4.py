8fb876a7ba25b8a272e16af558761e9e566fdcf7
Teerasitza <6629011035@cdti.ac.th>
2024-06-26 11:21:57 +0700

    update

---
+++ lab 4-4.py
@@ -0,0 +1,11 @@
+p = {
+    "name": "Teerasit sandee",
+    "age": 16,
+    "w": 60,
+    "h":173,
+    "email": "subnun3@gmail.com"
+    }
+
+print("สวัสดีครับคุณ %s" % p["name"])
+print("อายุ %d น้ำหนัก %d ส่วนสูง %d" % (p["age"], p["w"], p["h"]))
+print("อีเมล %s" % (p["email"]))
